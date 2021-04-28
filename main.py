6301b1c0fe980dfcf21431883a439322c9af7c0e
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2021-04-28 15:59:22 +0200

    Handling the input options

--- main.py
+++ main.py
@@ -5,6 +5,34 @@ IORequests = []
 head_ptr = 0
 
 
+def fcfs():
+    return
+
+
+def sstf():
+    return
+
+
+def scan():
+    return
+
+
+def c_scan():
+    return
+
+
+def look():
+    return
+
+
+def c_look():
+    return
+
+
+def newly_opt_alg():
+    return
+
+
 def main():
     global IORequests, head_ptr
 
@@ -19,10 +47,45 @@ def main():
     head_ptr = int(input("Head pointer: "))
 
     # Picking an algorithm
-    print("\nPick an algorithm's number:")
-    print("1- FCFS\n2- SSTF\n3- SCAN\n4- C-SCAN\n5- LOOK\n6- C-LOOK\n7- Newly optimized algorithm")
-    
-    alg = input("> ")
+    while True:
+        print("\nPick an algorithm's number:")
+        print("1- FCFS\n2- SSTF\n3- SCAN\n4- C-SCAN\n5- LOOK\n6- C-LOOK\n7- Newly optimized algorithm\n0- Exit")
+
+        # Handling non-numeric options
+        try:
+            op = int(input("> "))
+        except ValueError:
+            print("Unknown option")
+            continue
+
+        # Switching to the correct option
+        if op == 0:
+            print("Exiting..")
+            exit()
+
+        elif op == 1:
+            fcfs()
+
+        elif op == 2:
+            sstf()
+
+        elif op == 3:
+            scan()
+
+        elif op == 4:
+            c_scan()
+
+        elif op == 5:
+            look()
+
+        elif op == 6:
+            c_look()
+
+        elif op == 7:
+            newly_opt_alg()
+
+        else:
+            print("Unknown option")
 
 
 if __name__ == '__main__':
